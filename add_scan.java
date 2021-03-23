986acb9afd2931cf9d3e808c5930ba720faf7ff0
LovinduSaiGopinath <81213396+LovinduSaiGopinath@users.noreply.github.com>
2021-03-23 15:38:24 +0530

    add

---
+++ add_scan.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class add_scan{
+public static void main(String args[]) {
+Scanner sg=new Scanner(System.in);
+System.out.println("Enter the value of a");
+int a=sg.nextInt();
+System.out.println("Enter the value of b");
+int b=sg.nextInt();
+int c=a+b;
+System.out.println("Addition of a and are:"+c);
+}
+}
